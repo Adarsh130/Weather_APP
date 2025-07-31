5264671911e0972a94088410f00ec747ed7b21b6
Adarsh Paswan <112092468+Adarsh130@users.noreply.github.com>
2025-07-31 17:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,9 @@ def index():
     return render_template('index.html', weather=weather, history=history)
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+
 
